07f5ca5bf0f3f583909c6ad074357cbedf60cf5c
janpeterek <ja.peterek@seznam.cz>
2023-05-22 15:27:45 +0200

    02 Django

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Hrac
+from .models import Tym
+
 # Register your models here.
+
+admin.site.register(Hrac)
+admin.site.register(Tym)
+
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Hrac(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name='Jméno hráče',
+                            help_text='Zadej jméno hráče', )
+
+    country = models.CharField(max_length=50,
+                               verbose_name='Národnost',
+                               help_text='Zadej národnost hráče', )
+
+    born = models.DateField(verbose_name='Datum narození',
+                            help_text='Zadejte, prosím, datum ve formátu: <em>den.měsíc.rok</em>', )
+
+    class Meta:
+        verbose_name = 'Hráč'
+        verbose_name_plural = 'Hráči'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Tym(models.Model):
+    name = models.CharField(max_length=50,
+                            unique=True,
+                            verbose_name='Název týmu',
+                            help_text='Zadej název týmu', )
+
+    player = models.ManyToManyField(Hrac,
+                                    verbose_name='Zadej hráče',
+                                    help_text='Zadej jméno hráče',
+                                    )
+
+    class Meta:
+        verbose_name = 'Tým'
+        verbose_name_plural = 'Týmy'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
