d91d75f65353592b690ed0bec0f6e99eb0e123c4
janpeterek <78537460+janpeterek@users.noreply.github.com>
2023-06-14 14:27:26 +0200

    Add files via upload

--- movies/urls.py
+++ movies/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:pk>/', views.HracDetailView.as_view(), name='hrac')
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+
+from movies.models import Hrac
 
 
-def index(request): return render(request, "base.html", {})
+class HracDetailView(DetailView):
+    model = Hrac
+    context_object_name = 'hrac_detail'
+    template_name = 'hrac.html'
+
+
+def index(request):
+    popular_players = Hrac.objects.order_by('name')
+
+    context = {
+        'popular_players': popular_players,
+    }
+    return render(request, "index.html", context=context)
