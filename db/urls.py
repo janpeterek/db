ec0512f5fc821cc329788cef412f2c18d7524986
janpeterek <78537460+janpeterek@users.noreply.github.com>
2023-06-14 13:31:41 +0200

    Add files via upload

--- db/urls.py
+++ db/urls.py
@@ -15,15 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.admindocs import views
 from django.urls import path, include
 from django.views.generic import RedirectView
 from django.conf.urls.static import static
 from . import settings
 
+
 urlpatterns = [
-path('admin/', admin.site.urls),
-path('movies/', include('movies.urls')),
-path('', RedirectView.as_view(url='movies/')),
+    path('admin/', admin.site.urls),
+    path('', include('movies.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
